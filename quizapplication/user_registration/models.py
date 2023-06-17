f388a513f01b3112f2b002194dc0199a0356824c
nirenrawal <nire0007@stud.kea.dk>
2023-06-17 14:18:33 +0200

    userlist, view others profile added

--- quizapplication/user_registration/models.py
+++ quizapplication/user_registration/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="userinfo", on_delete=models.CASCADE)
     # address
     address = models.CharField(max_length=100, blank=True)
     # phone
